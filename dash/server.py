95fd02823e060dc67965692c78bad34826260e1b
Felipe A. Millacura <millacurafa@databio.tech>
2021-01-05 19:09:53 -0300

    Update server.py

--- dash/server.py
+++ dash/server.py
@@ -44,58 +44,58 @@ df= df.set_index('Fecha')
 
 #Checking for data correlations
 
-correlations = df[['Casos totales',
-                   'Casos recuperados',
-                   'Fallecidos',
-                   'Casos activos',
-                   'Casos nuevos totales',
-                  'Casos nuevos con sintomas',
-                  'Casos nuevos sin sintomas']].corr()
+#correlations = df[['Casos totales',
+#                   'Casos recuperados',
+#                   'Fallecidos',
+#                   'Casos activos',
+#                   'Casos nuevos totales',
+#                  'Casos nuevos con sintomas',
+#                  'Casos nuevos sin sintomas']].corr()
                    
-px.imshow(correlations)
+#px.imshow(correlations)
 
 #Ploting Symptomatic cases
 
-px.line(df['Casos nuevos con sintomas'],
-        y= 'Casos nuevos con sintomas',
-        title= "Asymptomatic cases",
-        labels= dict({'Casos nuevos con sintomas':'Number of Symptomatic cases',
-                     'Fecha':'Date'})
-        )
+#px.line(df['Casos nuevos con sintomas'],
+#        y= 'Casos nuevos con sintomas',
+#        title= "Asymptomatic cases",
+#        labels= dict({'Casos nuevos con sintomas':'Number of Symptomatic cases',
+#                     'Fecha':'Date'})
+#        )
 #Ploting Asymptomatic cases
-px.line(df['Casos nuevos sin sintomas'],
-        y= 'Casos nuevos sin sintomas',
-        title= "Asymptomatic cases",
-        labels= dict({'Casos nuevos sin sintomas':'Number of Asymptomatic cases',
-                     'Fecha':'Date'})
-        )
+#px.line(df['Casos nuevos sin sintomas'],
+#        y= 'Casos nuevos sin sintomas',
+#        title= "Asymptomatic cases",
+#        labels= dict({'Casos nuevos sin sintomas':'Number of Asymptomatic cases',
+#                     'Fecha':'Date'})
+#        )
 
 #Ploting Total new cases
 
-px.line(df['Casos nuevos totales'],
-        y= 'Casos nuevos totales',
-        title= "Daily cases",
-        labels= {'Casos nuevos totales':'Number of cases',
-                     'Fecha':'Date'}
-        )
+#px.line(df['Casos nuevos totales'],
+#        y= 'Casos nuevos totales',
+#        title= "Daily cases",
+#        labels= {'Casos nuevos totales':'Number of cases',
+#                     'Fecha':'Date'}
+#        )
 #Ploting Total Chilean cases
 
-px.line(df['Casos totales'],
-        y= 'Casos totales',
-        title= "Total Chilean cases",
-        labels= {'Fecha':'Date'}
-        ).update_layout(
-                    yaxis_title='Number of cases')
+#px.line(df['Casos totales'],
+#        y= 'Casos totales',
+#        title= "Total Chilean cases",
+#        labels= {'Fecha':'Date'}
+#        ).update_layout(
+#                    yaxis_title='Number of cases')
 
 # Total cases in logarithmic scales
 
-px.line(df['Casos totales'],
-        y= 'Casos totales',
-        title= "Total Chilean cases in logarithmic scale",
-        labels= dict({'Casos totales':'Log10(Number of cases)',
-                     'Fecha':'Date'}),
-        log_y = True
-        )    
+#px.line(df['Casos totales'],
+#        y= 'Casos totales',
+#        title= "Total Chilean cases in logarithmic scale",
+#        labels= dict({'Casos totales':'Log10(Number of cases)',
+#                     'Fecha':'Date'}),
+#        log_y = True
+#        )    
 # Adds PCR test data 
 pcr_cases = pd.read_csv('https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto7/PCR_T.csv')
 pcr_cases = pcr_cases.rename(columns={'Region':'Fecha'})
@@ -103,11 +103,11 @@ pcr_cases = pcr_cases.drop([0,1])
 pcr_cases = pcr_cases.set_index('Fecha')
 
 # Plots PCR cases
-px.line(pcr_cases,
-        title= "PCR test per region",
-        labels= {'Fecha':'Date'}
-        ).update_layout(
-                    yaxis_title='Number of PCR test')
+#px.line(pcr_cases,
+#        title= "PCR test per region",
+#       labels= {'Fecha':'Date'}
+#        ).update_layout(
+#                    yaxis_title='Number of PCR test')
 
 # Considering the total chilean population
 
@@ -118,31 +118,31 @@ N = 18300000 #Chilean population; Source: World Bank
 ## Infectious cases
 infectious = df[['Casos activos']].fillna(np.mean([13490,9990])) # Chilean active cases
 
-px.line(infectious,
-        title= "Infectious people",
-        labels= {'Fecha':'Date'},
-        ).update_layout(
-                    yaxis_title='Number of infectious cases')
+#px.line(infectious,
+#        title= "Infectious people",
+#        labels= {'Fecha':'Date'},
+#        ).update_layout(
+#                    yaxis_title='Number of infectious cases')
 
 ## Recovered Cases 1
 recovered1 = df['Casos recuperados'].dropna()
 
-px.line(recovered1,
-        title="Recovered cases"
-       ).update_layout(
-                        yaxis_title='Number of recovered cases',
-                        xaxis_title='Date'
-                       )
+#px.line(recovered1,
+#        title="Recovered cases"
+#       ).update_layout(
+#                        yaxis_title='Number of recovered cases',
+#                        xaxis_title='Date'
+#                       )
 # Recovered Cases 2
 recovered2 = df['Casos confirmados recuperados'].dropna()
 #df['Casos recuperados'] # Chilean recovered cases
 
-px.line(recovered2,
-        title="Recovered cases"
-       ).update_layout(
-                        yaxis_title='Number of recovered cases',
-                        xaxis_title='Date'
-                       )
+#px.line(recovered2,
+#        title="Recovered cases"
+#       ).update_layout(
+#                        yaxis_title='Number of recovered cases',
+#                        xaxis_title='Date'
+#                       )
 #Looking for recovered data from 1st to 20th June
 recovered3 = df.filter(like = '2020-06-', axis=0)['Casos recuperados por FD']
 
@@ -158,33 +158,33 @@ recovered3 = recovered3.drop(['2020-06-01',
                              '2020-06-29',
                              '2020-06-30'])
 
-px.line(recovered3,
-        title="Recovered cases"
-       ).update_layout(
-                        yaxis_title='Number of recovered cases',
-                        xaxis_title='Date'
-                       )
+#px.line(recovered3,
+#        title="Recovered cases"
+#       ).update_layout(
+#                        yaxis_title='Number of recovered cases',
+#                        xaxis_title='Date'
+#                       )
 ## Total Recovered cases
 
 recovered4 = pd.DataFrame(pd.concat([recovered1,recovered3,recovered2]
                                     ),
                          columns=['Casos recuperados totales'])
-px.line(recovered4,
-        title="Total number of recovered cases"
-       ).update_layout(
-                        yaxis_title='Number of recovered cases',
-                        xaxis_title='Date'
-                       ) 
+#px.line(recovered4,
+#        title="Total number of recovered cases"
+#       ).update_layout(
+#                        yaxis_title='Number of recovered cases',
+#                        xaxis_title='Date'
+#                       ) 
 
 ## Death cases
 deaths = df[['Fallecidos']].fillna(0) #death cases
 
-px.line(deaths,
-        title="Total number of deaths"
-       ).update_layout(
-                        yaxis_title='Number of deaths',
-                        xaxis_title='Date'
-                       )  
+#px.line(deaths,
+#        title="Total number of deaths"
+#       ).update_layout(
+#                        yaxis_title='Number of deaths',
+#                        xaxis_title='Date'
+#                       )  
 
 # Exposed cases
 
@@ -193,38 +193,38 @@ exposed = df[['Casos nuevos totales']].dropna()
 #For MA7
 ## exposed.rolling(7).mean()
 
-px.line(exposed,
-        title="Total number of exposed people"
-       ).update_layout(
-                        yaxis_title='Number of exposed people',
-                        xaxis_title='Date'
-                       )
+#px.line(exposed,
+#        title="Total number of exposed people"
+#       ).update_layout(
+#                        yaxis_title='Number of exposed people',
+#                        xaxis_title='Date'
+#                       )
 
 # Susceptible cases
 susceptible= pd.DataFrame()
 susceptible['Casos susceptibles totales'] = N -exposed['Casos nuevos totales']- infectious['Casos activos'] - recovered4['Casos recuperados totales']-deaths['Fallecidos']
 susceptible= pd.DataFrame(susceptible['Casos susceptibles totales'], columns=['Casos susceptibles totales'])
-px.line(susceptible,
-        title="Total number of susceptible people"
-       ).update_layout(
-                        yaxis_title='Number of susceptible people',
-                        xaxis_title='Date'
-                       )           
+#px.line(susceptible,
+#        title="Total number of susceptible people"
+#       ).update_layout(
+#                        yaxis_title='Number of susceptible people',
+#                        xaxis_title='Date'
+#                       )           
 
 # Defines plot for model
 
-def plotlyrealgo(S, E, I, R, D):
-    
-    fig = go.Figure()
-    fig.add_trace(go.Line(name="Susceptible", x=S.index, y=np.log10(S.iloc[:, 0]), line_color="dark blue"))
-    fig.add_trace(go.Line(name="Exposed", x=E.index, y=np.log10(E.iloc[:, 0]), line_color="gold"))
-    fig.add_trace(go.Line(name="Infectious", x=I.index, y=np.log10(I.iloc[:, 0]), line_color="red"))
-    fig.add_trace(go.Line(name="Recovered", x=R.index, y=np.log10(R.iloc[:, 0]), line_color="green"))
-    fig.add_trace(go.Line(name="Deaths", x=D.index, y=np.log10(D.iloc[:, 0]), line_color="black"))
-    fig.update_layout(title='SEIRD model real data',
-                      yaxis_title='SEIRD cases',
-                      xaxis_title='Date')
-    return fig.show()
+#def plotlyrealgo(S, E, I, R, D):
+#    
+#    fig = go.Figure()
+#    fig.add_trace(go.Line(name="Susceptible", x=S.index, y=np.log10(S.iloc[:, 0]), line_color="dark blue"))
+#    fig.add_trace(go.Line(name="Exposed", x=E.index, y=np.log10(E.iloc[:, 0]), line_color="gold"))
+#    fig.add_trace(go.Line(name="Infectious", x=I.index, y=np.log10(I.iloc[:, 0]), line_color="red"))
+#    fig.add_trace(go.Line(name="Recovered", x=R.index, y=np.log10(R.iloc[:, 0]), line_color="green"))
+#    fig.add_trace(go.Line(name="Deaths", x=D.index, y=np.log10(D.iloc[:, 0]), line_color="black"))
+#    fig.update_layout(title='SEIRD model real data',
+#                      yaxis_title='SEIRD cases',
+#                      xaxis_title='Date')
+#    return fig.show()
 
 # Plots SEIRD real
 
@@ -272,47 +272,47 @@ S, E, I, R, D = ret.T
 
 ## Plots SIR model
 
-def plotsir(t, S, I, R):
-      fig = go.Figure()
-      fig.add_trace(go.Line(name="Susceptible", x=t, y=S, line_color="dark blue"))
-     # fig.add_trace(go.Line(name="Exposed", x=t, y=E, line_color="gold"))
-      fig.add_trace(go.Line(name="Infectious", x=t, y=I, line_color="red"))
-      fig.add_trace(go.Line(name="Recovered", x=t, y=R, line_color="green"))
-    #  fig.add_trace(go.Line(name="Deaths", x=t, y=D, line_color="black"))
-      fig.update_layout(title='SIR model',
-                      yaxis_title='SIR cases',
-                      xaxis_title='Date')
-      return fig.show()
+#def plotsir(t, S, I, R):
+#      fig = go.Figure()
+#      fig.add_trace(go.Line(name="Susceptible", x=t, y=S, line_color="dark blue"))
+#      fig.add_trace(go.Line(name="Exposed", x=t, y=E, line_color="gold"))
+#      fig.add_trace(go.Line(name="Infectious", x=t, y=I, line_color="red"))
+#      fig.add_trace(go.Line(name="Recovered", x=t, y=R, line_color="green"))
+#      fig.add_trace(go.Line(name="Deaths", x=t, y=D, line_color="black"))
+#      fig.update_layout(title='SIR model',
+#                      yaxis_title='SIR cases',
+#                      xaxis_title='Date')
+#      return fig.show()
 
 #plotsir(t, S, I, R)
 
 ##Plots SEIR
 
-def plotseir(t, S, E, I, R):
-      fig = go.Figure()
-      fig.add_trace(go.Line(name="Susceptible", x=t, y=S, line_color="dark blue"))
-      fig.add_trace(go.Line(name="Exposed", x=t, y=E, line_color="gold"))
-      fig.add_trace(go.Line(name="Infectious", x=t, y=I, line_color="red"))
-      fig.add_trace(go.Line(name="Recovered", x=t, y=R, line_color="green"))
-    #  fig.add_trace(go.Line(name="Deaths", x=t, y=D, line_color="black"))
-      fig.update_layout(title='SEIR model',
-                      yaxis_title='SEIR cases',
-                      xaxis_title='Date')
-      return fig.show()
+#def plotseir(t, S, E, I, R):
+#      fig = go.Figure()
+#      fig.add_trace(go.Line(name="Susceptible", x=t, y=S, line_color="dark blue"))
+#      fig.add_trace(go.Line(name="Exposed", x=t, y=E, line_color="gold"))
+#      fig.add_trace(go.Line(name="Infectious", x=t, y=I, line_color="red"))
+#      fig.add_trace(go.Line(name="Recovered", x=t, y=R, line_color="green"))
+#      fig.add_trace(go.Line(name="Deaths", x=t, y=D, line_color="black"))
+#      fig.update_layout(title='SEIR model',
+#                      yaxis_title='SEIR cases',
+#                      xaxis_title='Date')
+#      return fig.show()
 #plotseir(t, S, E, I, R)
 
 ## Plots SEIRD
 
-def plotseird(t, S, E, I, R, D):
-      fig = go.Figure()
-      fig.add_trace(go.Line(name="Susceptible", x=t, y=S, line_color="dark blue"))
-      fig.add_trace(go.Line(name="Exposed", x=t, y=E, line_color="gold"))
-      fig.add_trace(go.Line(name="Infectious", x=t, y=I, line_color="red"))
-      fig.add_trace(go.Line(name="Recovered", x=t, y=R, line_color="green"))
-      fig.add_trace(go.Line(name="Deaths", x=t, y=D, line_color="black"))
-      fig.update_layout(title='SEIRD model',
-                      yaxis_title='SEIRD cases',
-                      xaxis_title='Date')
-      return fig.show()
+#def plotseird(t, S, E, I, R, D):
+#      fig = go.Figure()
+#      fig.add_trace(go.Line(name="Susceptible", x=t, y=S, line_color="dark blue"))
+#      fig.add_trace(go.Line(name="Exposed", x=t, y=E, line_color="gold"))
+#      fig.add_trace(go.Line(name="Infectious", x=t, y=I, line_color="red"))
+#      fig.add_trace(go.Line(name="Recovered", x=t, y=R, line_color="green"))
+#      fig.add_trace(go.Line(name="Deaths", x=t, y=D, line_color="black"))
+#      fig.update_layout(title='SEIRD model',
+#                      yaxis_title='SEIRD cases',
+#                      xaxis_title='Date')
+#      return fig.show()
 
 #plotseird(t, S, E, I, R, D)
